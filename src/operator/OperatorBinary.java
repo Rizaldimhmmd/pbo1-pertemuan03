524372e21dfe612755bd7141736eac3898a37340
Rizaldimhmmd <Rizaldimhmmd09@gmail.com>
2021-04-25 11:07:31 +0800

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,12 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (-) sebagai assigment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Muhammad Rizaldu";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.95;
+}
