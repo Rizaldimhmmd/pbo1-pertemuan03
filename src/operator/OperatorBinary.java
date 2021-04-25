5c7feb6103377d701316f734c3b31808617722de
Rizaldimhmmd <Rizaldimhmmd09@gmail.com>
2021-04-25 11:07:55 +0800

    Operator Binary Aritmatika

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -9,4 +9,13 @@ public class OperatorBinary {
     String nama = "Muhammad Rizaldu";
     int jumlahSks = 24;
     double indeksPrestasiKumulatif = 3.95;
+
+    public static void main(String[] args) {
+        System.out.println(9 + 3);
+        System.out.println(9 - 3);
+        System.out.println(9 * 3);
+        System.out.println(9 / 3);
+        System.out.println(9 % 3);
+        System.out.println(9 % 2);
+    }
 }
